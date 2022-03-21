e667e16b8e2e09b5f01a474dcc2001e2cd40f162
rezamandaa <rezamandaa@gmail.com>
2022-03-21 22:41:05 +0700

    Define the routes

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstScreen()
+      title: 'Named Routes Demo',
+      initialRoute: "/",
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/second':(context) => const SecondScreen()
+      },
     );
   }
 }
